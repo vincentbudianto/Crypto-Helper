4240908afdd7c977ec5e5f282e0d03c46617e686
Oksidian Tafly <oksitafly@gmail.com>
2020-09-04 23:26:37 +0800

    - Frontend Backend Integration

--- src/Components/Decrypt/Decrypt.js
+++ src/Components/Decrypt/Decrypt.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Select from "react-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import DecryptForm from './DecryptForm';
 import "./Decrypt.css";
+
+var aKVig = require("../../backend/autoKeyVigenere");
+var eVig = require("../../backend/extendedVigenere");
+var fVig = require("../../backend/fullVigenere");
+var playfair = require("../../backend/playfair");
+var sEnc = require("../../backend/superEncryption");
+var Vig = require("../../backend/vigenere");
 
 const selectStyles = {
   option: (provided, state) => ({
@@ -13,28 +19,80 @@ const selectStyles = {
 };
 
 const selectOptions = [
-  { value: "Vigenere Standard", label: "Vigenere Standard" },
-  { value: "Full Vigenere Cipher", label: "Full Vigenere Cipher" },
-  { value: "Auto-Key Vigenere Cipher", label: "Auto-Key Vigenere Cipher" },
-  { value: "Extended Vigenere Cipher", label: "Extended Vigenere Cipher" },
-  { value: "Playfair Cipher", label: "Playfair Cipher" },
-  { value: "Super Encription", label: "Super Encription" },
+  { value: Vig, label: "Vigenere Standard" },
+  { value: fVig, label: "Full Vigenere Cipher" },
+  { value: aKVig, label: "Auto-Key Vigenere Cipher" },
+  { value: eVig, label: "Extended Vigenere Cipher" },
+  { value: playfair, label: "Playfair Cipher" },
+  { value: sEnc, label: "Super Encription" },
   { value: "Affine Cipher", label: "Affine Cipher" },
   { value: "Hill Cipher", label: "Hill Cipher" },
-  { value: "Enigma Cipher", label: "Enigma Cipher" },
+  { value: "Enigma Cipher", label: "Enigma Cipher" }
 ];
+
+const truncate = (input) => {
+  return (input.length > 10) ? input.substr(0, 9) + '...' : input;
+}
+
+let fileReader;
 
 class Decrypt extends Component {
   state = {
     method: undefined,
     selectedFile: undefined,
-    text: undefined,
-    key: undefined,
+    fileName: "",
+    text: "",
+    key: "",
+  }
+
+  onTextChange = event => {
+    this.setState({ text: event.target.value })
+  }
+
+  onKeyChange = event => {
+    this.setState({ key: event.target.value })
+  }
+
+  onMethodChange = event => {
+
+    // Update the state
+    console.log(event)
+    this.setState({ method: event.value })
+  }
+
+  // On file select (from the pop up) 
+  onFileChange = event => { 
+     
+    // Update the state 
+    this.setState({ selectedFile: event.target.files[0] }); 
+    this.setState({ fileName: event.target.files[0].name })
+  };
+
+  handleFileRead = (e) => {
+    const content = fileReader.result;
+    console.log(content);
   }
 
   handleDecrypt = async (e) => {
     e.preventDefault();
     console.log("decrypt");
+    if (this.state.method !== undefined && this.state.key !== "") {
+      console.log(this.state.method)
+      if (this.state.text !== "") {
+      console.log(this.state.text)
+      console.log(this.state.key)
+      alert(this.state.method.decrypt(this.state.text, this.state.key)) // Encryption result
+      /**
+       * TODO : Connect to modal
+       */
+      }
+      else if (this.state.selectedFile !== undefined) {
+        console.log(this.state.selectedFile);
+        fileReader = new FileReader();
+        fileReader.onloadend = this.handleFileRead;
+        fileReader.readAsArrayBuffer(this.state.selectedFile);
+      }
+    }
   }
 
   download = () => {
@@ -70,10 +128,28 @@ class Decrypt extends Component {
             },
           })}
           options={selectOptions}
+          onChange={this.onMethodChange}
         />
         <div className="wrapper-decrypt">
           <div className="container-decrypt">
-            <DecryptForm onDecrypt={this.handleDecrypt} />
+            <form className="decrypt-form" onSubmit={this.handleDecrypt}>
+              <label htmlFor="text" id="label-account">Text</label>
+              <textarea id="text-input" type="text" name="text" rows="6" onChange={this.onTextChange} value={this.state.text}/>
+
+              <label htmlFor="key" id="label-key">Key</label>
+              <input id="key-input" type="text" name="key" onChange={this.onKeyChange} value={this.state.key}/>
+              <span id="false-key-msg" className="input-message"></span>
+
+              <div className="button-container">
+                <input id="file-input" type="file" name="file" className="upload-button" onChange={this.onFileChange} />
+                <label htmlFor="file-input">
+                  <FontAwesomeIcon icon="file-upload" /> {this.state.fileName === "" ? "Upload" : truncate(this.state.fileName)}
+                </label>
+                <button className="decrypt-button" type="submit">
+                  <FontAwesomeIcon icon="lock-open"/> &nbsp; Decrypt
+                </button>
+              </div>
+            </form>
           </div>
         </div>
         <div id="modal-result" className="modal-decrypt">
