f3c77452e7edfbb6162c00250927664b5fb643d4
vincentbudianto <vincent.budianto@gmail.com>
2020-09-08 00:27:10 +0700

    Updates

---
+++ precache-manifest.7d02ded3868a640164b9ab27ccd1ec80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f93581c8cf56b82827ec5c2a1b500e",
+    "url": "/Crypto-Helper/index.html"
+  },
+  {
+    "revision": "3cf352cd6c9e02ad64d8",
+    "url": "/Crypto-Helper/static/css/main.1593b777.chunk.css"
+  },
+  {
+    "revision": "1877bf3cb41a731a163d",
+    "url": "/Crypto-Helper/static/js/2.6bef8c2e.chunk.js"
+  },
+  {
+    "revision": "4ad464098713161b582f96eb9f0ed64f",
+    "url": "/Crypto-Helper/static/js/2.6bef8c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf352cd6c9e02ad64d8",
+    "url": "/Crypto-Helper/static/js/main.aa3a99ed.chunk.js"
+  },
+  {
+    "revision": "ecfec08d558ccdc5e3de",
+    "url": "/Crypto-Helper/static/js/runtime-main.48a7c6ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Crypto-Helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
